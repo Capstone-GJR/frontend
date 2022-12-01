8e164f78324dd93611f5be4b88d95e65b3c5a78a
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-12-01 16:18:50 -0600

    completed html for register component

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import Button from '../buttons/Button'
+import React from "react";
+import RegisterForm from "../forms/RegisterForm";
 
 function Register() {
-  return (
-    <div>
-        <h1>TRAQURA</h1>
-        <p>SIGN UP FOR A NEW ACCOUNT</p>
-        <Button title='SIGN UP'/>
-        <p>ALREADY HAVE AN ACCOUNT? SIGN IN</p>
-    </div>
-  )
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col text-center">
+                    <h1>TRAQURA</h1>
+                    <p className="my-4">SIGN UP FOR A NEW ACCOUNT</p>
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col col-lg-6 my-3">
+                    <RegisterForm />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col text-center">
+                    <p className="my-5">ALREADY HAVE AN ACCOUNT? SIGN IN</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Register
+export default Register;
