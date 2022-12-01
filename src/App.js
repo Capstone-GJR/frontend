3626510f889c787edea000e5f1e93b22a54bddf7
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-12-01 13:06:25 -0600

    created register component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import {
   Route, 
 } from 'react-router-dom'
 import Welcome from './components/pages/Welcome';
+import Register from './components/pages/Register';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Welcome />}/> 
+        <Route path='/register' element={<Register />}/> 
       </Routes>
     </Router>
   );
---
+++ src/components/pages/Register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '../buttons/Button'
+
+function Register() {
+  return (
+    <div>
+        <h1>TRAQURA</h1>
+        <p>SIGN UP FOR A NEW ACCOUNT</p>
+        <Button title='SIGN UP'/>
+        <p>ALREADY HAVE AN ACCOUNT? SIGN IN</p>
+    </div>
+  )
+}
+
+export default Register
