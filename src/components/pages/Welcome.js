d402e6a5557701a5340da929b54cf4ec6821631b
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-12-01 17:34:18 -0600

    welcome page edits - added navigation to register page, deleted welcome page css and replaced with bootstrap

--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from '../buttons/Button';
-import welcome from './welcome.module.css';
 
 function Home() {
   return (
-    <div>
-      <div className={welcome.container}>
-        <h1>TRAQURA</h1>
-        <div className={welcome.btnContainer}>
-          <Button title="LOGIN" />
-          <Button title="NEW ACCOUNT?" />
+    <div className='container'>
+      <div className='row' style={{marginBottom:"25em"}}>
+        <div className='col text-center'>
+          <h1>TRAQURA</h1>
         </div>
-      </div> 
+      </div>
+      <div className='row justify-content-center my-5'>
+        <div className='col col-lg-6'>
+          <Button title="LOGIN" />
+        </div>
+      </div>
+      <div className='row justify-content-center'>
+        <div className='col col-lg-6'>
+          <Link to='/register'>
+            <Button title="NEW ACCOUNT?"  />
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
