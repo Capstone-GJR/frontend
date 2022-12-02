09c53d0c9ea8375f1a0c1d775c4d2524c3a700e5
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-12-01 18:38:42 -0600

    added all elements to login page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import LoginForm from '../forms/LoginForm';
 
 function Login() {
   return (
     <div className='container'>
+        <h1>TRAQURA</h1>
         <LoginForm />
+        <Link to='/register' style={{textDecoration: 'none'}}>
+            <p className='lightGrey text-center'>NO ACCOUNT? SIGN UP!</p>
+        </Link>
     </div>
   )
 }
