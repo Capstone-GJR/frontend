9f32c835c765e1d96bc80f122dbae6317d31feb8
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-12-04 16:51:34 -0600

    created a private route for pages

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import Form from "react-bootstrap/Form";
 import { Link } from 'react-router-dom';
+// import { useLocalState } from '../../util/useLocalState';
 import Button from '../buttons/Button';
 
 function LoginForm() {
+
+  // const [jwt, setJwt] = useLocalState("", "jwt");
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
