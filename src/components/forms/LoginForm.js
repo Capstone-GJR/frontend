02d265b0f8c57faac1ffa62a610a80f918a8fc12
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-12-03 18:30:49 -0600

    added useState hooks to capture user input

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from "react-bootstrap/Form";
 import { Link } from 'react-router-dom';
 import Button from '../buttons/Button';
 
 function LoginForm() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('email: ', email);
+    console.log('password: ', password);
+  }
+
   return (
     <Form>
-        <Form.Group className="mb-4" controlId="formBasicEmail">
-            <Form.Control className="p-3 inputText" type="text" placeholder="Username" />
+        <Form.Group className="mb-4" controlId="email">
+            <Form.Control 
+              className="p-3 inputText" 
+              type="text" 
+              placeholder="Username (Email)" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
         </Form.Group>
 
-        <Form.Group className="mb-4" controlId="formBasicPassword">
-            <Form.Control className="p-3 inputText" type="password" placeholder="Password" />
+        <Form.Group className="mb-4" controlId="password">
+            <Form.Control 
+              className="p-3 inputText" 
+              type="password" 
+              placeholder="Password" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              />
         </Form.Group>
 
         <Link to='#' className='customLink'>
             <p className='grayText'>Forgot password?</p>
         </Link>
 
-        <Button title="LOGIN" />
+        <Button title="LOGIN" onClick={handleSubmit} />
     </Form>
   )
 }
