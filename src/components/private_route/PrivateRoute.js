a54842ce144178f30239136b49e81f59e907f457
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-12-05 18:44:45 -0600

    updated privateRoute to redirect if jwt token is not in local storage

--- src/components/private_route/PrivateRoute.js
+++ src/components/private_route/PrivateRoute.js
@@ -3,8 +3,13 @@ import { Navigate } from 'react-router-dom';
 import { useLocalState } from '../util/useLocalState'
 
 function PrivateRoute({ children }) {
-    const [jwt, setJwt] = useLocalState("", "jwt");
-    return jwt ? children : <Navigate to="/login" />
+    const access_token = localStorage.getItem("access_token");
+
+    if(!access_token) {
+        return <Navigate to="/login" />
+    }
+
+    return children;
 }
 
 export default PrivateRoute;
