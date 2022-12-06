23b9ddd0ad478b442760d3338bc9f19d831c210d
Ramaj Johnson <rjohnson3795@gmail.com>
2022-12-06 13:32:34 -0600

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,32 +7,32 @@ function Navbar(props) {
     return (
        <div className="container p-0 fixed-bottom">
            <nav className="navbar navbar-dark bg-light p-0">
-               <div className="container-fluid p-0 border border-dark bg-dark justify-content-around">
+               <div className="container-fluid p-0 border border-dark bg-dark d-flex justify-content-evenly">
                    <div className="row">
-                       <div className="col p-0">
-                           <button className="btn btn-dark">
+                       <div className="col p-0 m-auto">
+                           <button className="btn btn-dark m-auto">
                                {/*<CiBoxes />*/}
                                Spaces
                            </button>
                        </div>
-                       <div className="col p-0">
-                           <button className="btn btn-dark">
-                               Spaces
+                       <div className="col p-0 m-auto">
+                           <button className="btn btn-dark m-auto">
+                               Items
                            </button>
                        </div>
-                       <div className="col p-0">
-                           <button className="btn btn-dark">
-                               Spaces
+                       <div className="col p-0 m-auto">
+                           <button className="btn btn-dark m-auto">
+                               Scan
                            </button>
                        </div>
-                       <div className="col p-0">
-                           <button className="btn btn-dark">
-                               Spaces
+                       <div className="col p-0 m-auto">
+                           <button className="btn btn-dark m-auto">
+                               Search
                            </button>
                        </div>
-                       <div className="col p-0">
-                           <button className="btn btn-dark">
-                               Spaces
+                       <div className="col p-0 m-auto">
+                           <button className="btn btn-dark m-auto">
+                               Settings
                            </button>
                        </div>
                    </div>
