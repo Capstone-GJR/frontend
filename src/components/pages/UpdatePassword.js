d5a7d7eba0eb0969a23db53f9f9fc4fb5d10fbbd
Ramaj Johnson <rjohnson3795@gmail.com>
2022-12-07 10:40:10 -0600

    navbar added

---
+++ src/components/pages/UpdatePassword.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TopNavbar from "../navbar/TopNavbar";
+import BottomNavbar from "../navbar/BottomNavbar";
+
+function UpdatePassword(props){
+    return (
+        <div>
+            <TopNavbar/>
+            <div>Delete Box</div>
+            <BottomNavbar/>
+        </div>
+
+    )
+}
+
+export default UpdatePassword
