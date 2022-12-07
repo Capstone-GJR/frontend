1df6553bafaa92da3eb01942cb899873c82469d3
Ramaj Johnson <rjohnson3795@gmail.com>
2022-12-07 10:32:11 -0600

    navbar added

---
+++ src/components/pages/BoxSettings.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TopNavbar from "../navbar/TopNavbar";
+import BottomNavbar from "../navbar/BottomNavbar";
+
+function BoxSettings(props){
+    return (
+        <div>
+            <TopNavbar/>
+            <div>Add Box</div>
+            <BottomNavbar/>
+        </div>
+
+    )
+}
+
+export default BoxSettings
