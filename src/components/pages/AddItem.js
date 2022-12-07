6f90dd61f40d3284419fc5f7324db23ad90820c8
Ramaj Johnson <rjohnson3795@gmail.com>
2022-12-07 10:27:48 -0600

    navbar added

---
+++ src/components/pages/AddItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TopNavbar from "../navbar/TopNavbar";
+import BottomNavbar from "../navbar/BottomNavbar";
+
+function AddItem(props){
+    return (
+        <div>
+            <TopNavbar/>
+            <div>Add Item</div>
+            <BottomNavbar/>
+        </div>
+
+    )
+}
+
+export default AddItem
