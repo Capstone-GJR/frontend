e4eb6d692f64f4ac578c3e72fd8cfe3355ad6c39
Ramaj Johnson <rjohnson3795@gmail.com>
2022-12-07 10:35:53 -0600

    navbar added

---
+++ src/components/pages/DeleteUser.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TopNavbar from "../navbar/TopNavbar";
+import BottomNavbar from "../navbar/BottomNavbar";
+
+function DeleteUser(props){
+    return (
+        <div>
+            <TopNavbar/>
+            <div>Delete User</div>
+            <BottomNavbar/>
+        </div>
+
+    )
+}
+
+export default DeleteUser
