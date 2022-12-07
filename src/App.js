0be9936a5ebb709b0d9ba4dd301b6438f18d89f7
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-07 12:43:02 -0600

    Name changes and space landing

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import Welcome from './components/pages/welcome/Welcome';
 import Register from './components/pages/welcome/Register';
 import Login from './components/pages/welcome/Login';
 import PrivateRoute from './components/private_route/PrivateRoute';
-import UserLanding from './components/pages/user-crud/UserLanding';
+import AllSpaces from './components/pages/user-crud/AllSpaces';
 import AboutUs from "./components/pages/welcome/AboutUs";
 import Items from "./components/pages/Items";
 import Scan from "./components/pages/Scan";
 import Search from "./components/pages/Search";
 import Profile from "./components/pages/Profile";
+import SpaceLanding from "./components/pages/space-crud/SpaceLanding";
 
 function App() {
   return (
@@ -25,14 +26,19 @@ function App() {
         <Route path='/login' element={<Login />}/> 
         <Route path='/aboutus' element={<AboutUs />}/>
 
-        <Route path='/userlanding' element={
+        <Route path='/allSpaces' element={
           <PrivateRoute>
-            <UserLanding />
+            <AllSpaces />
           </PrivateRoute> }>
         </Route>
         <Route path='/profile' element={
           <PrivateRoute>
             <Profile />
+          </PrivateRoute> }>
+        </Route>
+        <Route path='/spaceLanding' element={
+          <PrivateRoute>
+            <SpaceLanding />
           </PrivateRoute> }>
         </Route>
 
--- src/components/pages/space-crud/SpaceLanding.js
+++ src/components/pages/space-crud/SpaceLanding.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import TopNavbar from "../../navbar/TopNavbar";
 import BottomNavbar from "../../navbar/BottomNavbar";
+import {useLocation} from "react-router-dom";
 
 function SpaceLanding(props){
+    const location = useLocation()
+    console.log(location)
     return (
         <div>
             <TopNavbar/>
             <div>Space Landing</div>
+            <div>space id: {location.state.space_id}</div>
             <BottomNavbar/>
         </div>
 
