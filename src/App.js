caceb58375f312175422c0d62d3476aa85d28f8e
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-08 08:48:24 -0600

    Fixed routing issue

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
 import './App.css';
-import { 
-  BrowserRouter as Router, 
+import {
+  BrowserRouter as Router,
   Routes,
-  Route, 
+  Route,
 } from 'react-router-dom'
 import Welcome from './components/pages/welcome/Welcome';
 import Register from './components/pages/welcome/Register';
 import Login from './components/pages/welcome/Login';
 import PrivateRoute from './components/private_route/PrivateRoute';
-import UserLanding from './components/pages/user-crud/UserLanding';
+import AllSpaces from './components/pages/user-crud/AllSpaces';
 import AboutUs from "./components/pages/welcome/AboutUs";
 import Items from "./components/pages/Items";
 import Scan from "./components/pages/Scan";
 import Search from "./components/pages/Search";
-import Profile from "./components/pages/Profile";
 import SpaceLanding from "./components/pages/space-crud/SpaceLanding";
 import ToteLanding from './components/pages/box-crud/ToteLanding';
 import ItemLanding from './components/pages/item-crud/ItemLanding';
+import UserProfile from "./components/pages/UserProfile";
 
 function App() {
   return (
-    <Router>
-      <Routes>
+      <Router>
+        <Routes>
 
-        <Route path='/' element={<Welcome />}/> 
-        <Route path='/register' element={<Register />}/> 
-        <Route path='/login' element={<Login />}/> 
-        <Route path='/aboutus' element={<AboutUs />}/>
+          <Route path='/' element={<Welcome />}/>
+          <Route path='/register' element={<Register />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/aboutus' element={<AboutUs />}/>
 
-        <Route path='/userlanding' element={
-          <PrivateRoute>
-            <UserLanding />
-          </PrivateRoute> }>
-        </Route>
-        <Route path='/profile' element={
-          <PrivateRoute>
-            <Profile />
-          </PrivateRoute> }>
-        </Route>
+          <Route path='/allSpaces' element={
+            <PrivateRoute>
+              <AllSpaces />
+            </PrivateRoute> }>
+          </Route>
+          <Route path='/profile' element={
+            <PrivateRoute>
+              <UserProfile />
+            </PrivateRoute> }>
+          </Route>
+          <Route path='/spaceLanding' element={
+            <PrivateRoute>
+              <SpaceLanding />
+            </PrivateRoute> }>
+          </Route>
+          <Route path='/toteLanding' element={
+            <PrivateRoute>
+              <ToteLanding />
+            </PrivateRoute> }>
+          </Route>
+          <Route path='/itemLanding' element={
+            <PrivateRoute>
+              <ItemLanding />
+            </PrivateRoute> }>
+          </Route>
 
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
   );
 }
 
