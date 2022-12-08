8ba08a4191aea8b58626e9ad69a15da025dc23de
Ramaj Johnson <rjohnson3795@gmail.com>
2022-12-08 10:58:04 -0600

    form component

--- src/components/forms/AddForm.js
+++ src/components/forms/AddForm.js
@@ -3,6 +3,7 @@ import Form from "react-bootstrap/Form";
 import NameField from "./input-fields/NameField";
 import ColorField from "./input-fields/ColorField";
 import KeywordsField from "./input-fields/KeywordsField";
+import ImageField from "./input-fields/ImageField";
 
 function AddForm(props){
     return (
@@ -18,6 +19,12 @@ function AddForm(props){
                 placeholder="Choose A Color"
                 value={form.color}
                 onChange={(e) => setField("color", e.target.value)}
+            />
+            <ImageField
+                type="file"
+                placeholder="Select An Image"
+                value={form.fileStackUrl}
+                onChange={(e) => setField("fileStackUrl", e.target.value)}
             />
             <KeywordsField
                 type="textarea"
---
+++ src/components/forms/input-fields/ImageField.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import Form from "react-bootstrap/Form";
+
+
+function ImageField(props){
+    return (
+        <Form.Group className="mb-4" controlId="color">
+            <Form.Control className="p-3 inputText"
+                          type={props.type}
+                          placeholder={props.placeholder}
+                          value={props.value}
+                          onChange={props.onChange}
+                          isInvalid={props.isInvalid}
+            />
+            {/*<Form.Control.Feedback type="invalid" className="text-center">*/}
+            {/*    {props.errorMsg}*/}
+            {/*</Form.Control.Feedback>*/}
+        </Form.Group>
+    )
+}
+
+export default ImageField
