fad882522f29cccf90731509199360f1b6064202
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-08 09:36:08 -0600

    Item Details display on Item Details page

--- src/components/pages/item-crud/ItemLanding.js
+++ src/components/pages/item-crud/ItemLanding.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import TopNavbar from "../../navbar/TopNavbar";
 import BottomNavbar from "../../navbar/BottomNavbar";
 import LargeNavbar from "../../navbar/LargeNavbar";
+import {useLocation} from "react-router-dom";
+import axios from "axios";
+import {AuthZHeader} from "../../util/HelperFunctions";
 
 function ItemLanding(props){
+
+    const [itemDetails, setItemDetails] = useState({});
+    const location = useLocation();
+    const endPoint = `/item/${location.state.item_id}`;
+
+    useEffect(() => {
+
+        const getItemDetails = async () => {
+            try {
+                const response = await axios.get(endPoint, AuthZHeader())
+                setItemDetails(response.data);
+                console.log(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getItemDetails();
+    },[])
+
     return (
         <div>
             <LargeNavbar />
-
             <TopNavbar/>
-            <div>Item Landing</div>
+            <h1>{location.state.item_name}</h1>
+            <div>
+                <div>Description: {itemDetails.keywords}</div>
+                <div>Value: ${itemDetails.value}</div>
+                <div>Space: {itemDetails.space.name}</div>
+                <div>Tote: {itemDetails.tote.name}</div>
+            </div>
             <BottomNavbar/>
         </div>
 
