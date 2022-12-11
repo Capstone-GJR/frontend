78d3e828d3bd50b03cde02949ff01f6ec18d093d
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-10 20:28:48 -0600

    created validate form helper function

--- src/components/util/HelperFunctions.js
+++ src/components/util/HelperFunctions.js
@@ -29,3 +29,18 @@ export const axiosPost = async (e, endPoint, obj) => {
         return err
     }
 }
+
+export const validateForm = (form) => {
+    const { firstName, lastName, email } = form;
+    const newErrors = {}
+
+    const regex = /\S+@\S+\.\S+/;
+    const blankErrorMsg = 'Field can not be blank';
+    const emailErrorMsg = 'Please enter a valid email';
+
+    if(!firstName || firstName === '') newErrors.firstName = blankErrorMsg
+    else if (!lastName || lastName === '') newErrors.lastName = blankErrorMsg
+    else if (!email || email === '' || !regex.test(email)) newErrors.email = emailErrorMsg
+
+    return newErrors
+}
