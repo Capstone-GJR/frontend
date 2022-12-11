8a83e6be5fc46a4881ba7c27c4424b28f40e4502
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-11 14:18:36 -0600

    styled updateUser page, added form useState to capture user input

--- src/components/forms/FormInput.js
+++ src/components/forms/FormInput.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Form from "react-bootstrap/Form"
 
-function FormInput(
-  {
+function FormInput({
     id, 
     label,
     type, 
@@ -11,27 +10,26 @@ function FormInput(
     onChange, 
     isInvalid, 
     errorMsg
-  }
-){
+  })
+{
 
   return (
-      <Form.Group  
-        className="my-2" 
-        controlId={id}
-        style={{maxWidth:600}}
-      >
-        <Form.Label className="p-1 mx-3">{label}</Form.Label>
-        <Form.Control className="p-3 inputText" 
-          type={type} 
-          placeholder={placeholder} 
-          value={value}
-          onChange={onChange}
-          isInvalid={isInvalid}
-        />
-        <Form.Control.Feedback type="invalid" className="text-center">
-          {errorMsg}
-        </Form.Control.Feedback>
-      </Form.Group>
+    <Form.Group  
+      className="my-3" 
+      controlId={id}
+    >
+      <Form.Label className="p-1 mx-3">{label}</Form.Label>
+      <Form.Control className="p-3 inputText" 
+        type={type} 
+        placeholder={placeholder} 
+        value={value}
+        onChange={onChange}
+        isInvalid={isInvalid}
+      />
+      <Form.Control.Feedback type="invalid" className="text-center">
+        {errorMsg}
+      </Form.Control.Feedback>
+    </Form.Group>
   )
 }
 
--- src/components/pages/user-crud/UpdateUser.js
+++ src/components/pages/user-crud/UpdateUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopNavbar from "../../navbar/TopNavbar";
 import BottomNavbar from "../../navbar/BottomNavbar";
 import LargeNavbar from "../../navbar/LargeNavbar";
@@ -10,15 +10,24 @@ import axios from 'axios';
 import { AuthZHeader } from '../../util/HelperFunctions';
 
 function UpdateUser(){
+    const [form, setForm] = useState({
+        firstName:'',
+        lastName:'',
+        email:''
+    });
+
     const location = useLocation();
+
+    const setField = (field, value) => {
+        setForm({
+            ...form,
+            [field]:value
+        });
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.put(`user/edit/${location.state.profile.id}`, { 
-            firstName: 'testFirstName',
-            lastName: 'testLastName',
-            email: 'testemail'
-        }, AuthZHeader())
+        axios.put(`user/edit/${location.state.profile.id}`, form , AuthZHeader())
         .then((res) => {
             console.log(res); 
         })
@@ -27,45 +36,48 @@ function UpdateUser(){
         });
     }
     
-
     return (
         <>
             <LargeNavbar />
             <TopNavbar/>
-            <h1>Update your Profile</h1>
-            <Form className='d-flex flex-column justify-items-center'>
-                <FormInput
-                    // id=
-                    label="FIRST NAME"
-                    type= "text"
-                    placeholder= {location.state.profile.firstName}
-                    // value=
-                    // onChange=
-                    // isInvalid=
-                    // errorMsg=
-                />
-                <FormInput
-                    // id=
-                    label="LAST NAME"
-                    type= "text"
-                    placeholder= {location.state.profile.lastName}
-                    // value=
-                    // onChange=
-                    // isInvalid=
-                    // errorMsg=
-                />
-                <FormInput
-                    id="test"
-                    label="EMAIL"
-                    type= "text"
-                    placeholder= {location.state.profile.email}
-                    // value=
-                    // onChange=
-                    // isInvalid=
-                    // errorMsg=
-                />
-                <Button title='Submit Edit' onClick={handleSubmit} />
-            </Form>
+            <div className='container'>
+                <h2 className='text-center m-3'>Update your Profile</h2>
+                <div className='maxWidth600 margin-0-Auto'>
+                    <Form>
+                        <FormInput
+                            // id=
+                            label="FIRST NAME"
+                            type= "text"
+                            placeholder= {location.state.profile.firstName}
+                            value={form.firstName}
+                            onChange={(e) => setField("firstName", e.target.value)}
+                            // isInvalid=
+                            // errorMsg=
+                        />
+                        <FormInput
+                            // id=
+                            label="LAST NAME"
+                            type= "text"
+                            placeholder= {location.state.profile.lastName}
+                            value={form.lastName}
+                            onChange={(e) => setField("lastName", e.target.value)}
+                            // isInvalid=
+                            // errorMsg=
+                        />
+                        <FormInput
+                            id="test"
+                            label="EMAIL"
+                            type= "text"
+                            placeholder= {location.state.profile.email}
+                            value={form.email}
+                            onChange={(e) => setField("email", e.target.value)}
+                            // isInvalid=
+                            // errorMsg=
+                        />
+                        <Button title='Submit Edit' onClick={handleSubmit} />
+                    </Form>
+                </div>
+            </div>
             <BottomNavbar/>
         </>
     )
