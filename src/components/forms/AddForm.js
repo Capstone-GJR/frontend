db2fc5bf11cb19046f6bf8040b9081615ee5cc29
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-14 16:32:08 -0600

    AddForm is fixed! Works to send post request even with old (but valid!) JWT token

--- src/components/forms/AddForm.js
+++ src/components/forms/AddForm.js
@@ -3,9 +3,7 @@ import Form from "react-bootstrap/Form";
 import NameField from "./input-fields/NameField";
 import ColorField from "./input-fields/ColorField";
 import KeywordsField from "./input-fields/KeywordsField";
-import ImageField from "./input-fields/ImageField";
 import {useNavigate} from "react-router-dom";
-// import {useState} from "@types/react";
 import Button from "../buttons/Button";
 import {AuthZHeader, axiosPost} from "../util/HelperFunctions";
 import axios from "axios";
@@ -25,7 +23,8 @@ function AddForm(props){
     const handleSubmit = async (e) => {
         e.preventDefault()
         try{
-            const response =  await axios.post(`/space/add`, form, AuthZHeader)
+            const response =  await axios.post(`/space/add`, form, AuthZHeader())
+            console.log(AuthZHeader)
             console.log(response)
             setForm({})
             navigate("/allSpaces")
@@ -56,12 +55,12 @@ function AddForm(props){
                 <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#filePicker">
                     File picker
                 </button>
-                <div className="modal fade" id="filePicker" tabIndex="-1" aria-labelledby="exampleModalLabel"
+                <div className="modal fade" id="filePicker" tabIndex="-1" aria-labelledby="filePicker"
                      aria-hidden="true">
                     <div className="modal-dialog">
                         <div className="modal-content">
                             <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="exampleModalLabel">Choose an image</h1>
+                                <h1 className="modal-title fs-5" id="filePickerLabel">Choose an image</h1>
                                 <button type="button" className="btn-close" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                             </div>
