f1624c47627b54a15ed5883b958a756b0c0eba20
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-14 11:57:11 -0600

    FilePicker.js work

--- src/components/pages/FilePicker.js
+++ src/components/pages/FilePicker.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import {PickerInline, PickerOverlay} from 'filestack-react';
 
 function FilePicker(props) {
+    const [imageUrl, setImageUrl] = useState('')
 
 
     return (
         <div>
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#filePicker">
+                File picker
             </button>
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
+            <div className="modal fade" id="filePicker" tabIndex="-1" aria-labelledby="exampleModalLabel"
                  aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Choose an image</h1>
                             <button type="button" className="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
                         </div>
                         {/*----------PICKER----------------*/}
                         <PickerInline
-                            apikey={'KEY_GOES_HERE'}
-                            // onSuccess={(res) => console.log(res)}
-                            onUploadDone={(res) => console.log(res.filesUploaded[0].url)}
+                            apikey={'A2vZPoGIoRiePhI4DbTFcz'}
+                            onUploadDone={ (res) => {
+                                console.log(res.filesUploaded[0].url)
+                                setImageUrl(res.filesUploaded[0].url)
+                                console.log(imageUrl)
+                            }}
+                            onSuccess={(res) => console.log(res)}
                         />
                     {/*    ------------------------------------*/}
                     </div>
--- src/components/pages/space-crud/AddSpace.js
+++ src/components/pages/space-crud/AddSpace.js
@@ -3,8 +3,6 @@ import TopNavbar from "../../navbar/TopNavbar";
 import BottomNavbar from "../../navbar/BottomNavbar";
 import LargeNavbar from "../../navbar/LargeNavbar";
 import AddForm from "../../forms/AddForm";
-import Button from "../../buttons/Button";
-import filePicker from "../FilePicker";
 import FilePicker from "../FilePicker";
 
 
