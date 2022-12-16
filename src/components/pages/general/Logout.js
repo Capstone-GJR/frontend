20ea456a1d235e62ed77f19fc81434305a02f8f2
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-16 05:00:00 -0600

    Add About us to logout page

--- src/components/pages/general/Logout.js
+++ src/components/pages/general/Logout.js
@@ -1,18 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Button from '../../buttons/Button';
 
 function Logout() {
-  return (
-    <div className='pageContainer'>
-        <h1>
-            You have been logged out.
-        </h1>
-        <Link to="/login">
-            <Button title="LOGIN" />
-        </Link>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <div className='row justify-content-center my-2'>
+                <h1>Thanks for visiting Traqura</h1>
+            </div>
+            <div className="py-5">
+                <div className='row justify-content-center pt-5 my-2'>
+                    <div className='col col-lg-6'>
+                        <Link to='/login'>
+                            <Button title="LOGIN"/>
+                        </Link>
+                    </div>
+                </div>
+                <div className='row justify-content-center my-2'>
+                    <div className='col col-lg-6'>
+                        <Link to='/register'>
+                            <Button title="REGISTER"/>
+                        </Link>
+                    </div>
+                </div>
+                <div className='row justify-content-center my-2'>
+                    <div className='col col-lg-6'>
+                        <Link to='/aboutUs'>
+                            <Button title="ABOUT US "/>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Logout
--- src/components/pages/general/Welcome.js
+++ src/components/pages/general/Welcome.js
@@ -5,26 +5,26 @@ import Button from '../../buttons/Button';
 function Welcome() {
   return (
     <div className='container'>
-      <div className='row' style={{marginBottom:"25em"}}>
+      <div className='row header' style={{marginBottom:"25em"}}>
         <div className='col text-center'>
           <h1>TRAQURA</h1>
         </div>
       </div>
-      <div className='row justify-content-center my-5'>
+      <div className='row justify-content-center my-2'>
         <div className='col col-lg-6'>
           <Link to='/login'>
             <Button title="LOGIN" />
           </Link>
         </div>
       </div>
-      <div className='row justify-content-center'>
+      <div className='row justify-content-center my-2'>
         <div className='col col-lg-6'>
           <Link to='/register'>
-            <Button title="NEW ACCOUNT?"  />
+            <Button title="REGISTER"  />
           </Link>
         </div>
       </div>
-        <div className='row justify-content-center'>
+        <div className='row justify-content-center my-2'>
             <div className='col col-lg-6'>
                 <Link to='/aboutus'>
                     <Button title="ABOUT US"  />
