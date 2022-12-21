9671172871db9c89da66480088b3100cc163f517
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-21 10:07:48 -0600

    Switch async authenticate request back to .then

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -24,54 +24,54 @@ function LoginForm() {
     setPassError("");
   }
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //     axios
-  //       .post
-  //         ('/authenticate', {
-  //           "email": form.email,
-  //           "password": form.password
-  //         })
-  //         .then((res) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      axios
+        .post
+          ('/authenticate', {
+            "email": form.email,
+            "password": form.password
+          })
+          .then((res) => {
             // Private route to access private pages
-            // localStorage.setItem("access_token", res.headers.authorization);
-            // // Global axios defaults
-            // axios.defaults.headers.common['Authorization'] = res.headers.authorization;
-            // navigate("/allSpaces");
-            // setForm({email: "", password: ""});
-  //           console.log(res.data)
-  //         })
-  //         .catch((error) => {
-  //             console.log(error)
-  //           const errorMsg = error.response.data;
-  //           if(errorMsg === "Username not found") setEmailError(errorMsg)
-  //           if(errorMsg === "Incorrect password provided") setPassError(errorMsg)
-  //         })
-  // }
-
-  const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-          const res = await axios
-              .post
-              ('/authenticate', {
-                  "email": form.email,
-                  "password": form.password
-              })
-          console.log(res)
-          // Private route to access private pages
-          localStorage.setItem("access_token", res.headers.authorization);
-          // Global axios defaults
-          axios.defaults.headers.common['Authorization'] = res.headers.authorization;
-          navigate("/allSpaces");
-          setForm({email: "", password: ""});
-      } catch (error) {
-          console.log(error)
-          const errorMsg = error.response.data;
-          if(errorMsg === "Username not found") setEmailError(errorMsg)
-          if(errorMsg === "Incorrect password provided") setPassError(errorMsg)
-      }
+            localStorage.setItem("access_token", res.headers.authorization);
+            // Global axios defaults
+            axios.defaults.headers.common['Authorization'] = res.headers.authorization;
+            navigate("/allSpaces");
+            setForm({email: "", password: ""});
+            console.log(res.data)
+          })
+          .catch((error) => {
+              console.log(error)
+            const errorMsg = error.response.data;
+            if(errorMsg === "Username not found") setEmailError(errorMsg)
+            if(errorMsg === "Incorrect password provided") setPassError(errorMsg)
+          })
   }
+
+  // const handleSubmit = async (e) => {
+  //     e.preventDefault();
+  //     try {
+  //         const res = await axios
+  //             .post
+  //             ('/authenticate', {
+  //                 "email": form.email,
+  //                 "password": form.password
+  //             })
+  //         console.log(res)
+  //         // Private route to access private pages
+  //         localStorage.setItem("access_token", res.headers.authorization);
+  //         // Global axios defaults
+  //         axios.defaults.headers.common['Authorization'] = res.headers.authorization;
+  //         navigate("/allSpaces");
+  //         setForm({email: "", password: ""});
+  //     } catch (error) {
+  //         console.log(error)
+  //         const errorMsg = error.response.data;
+  //         if(errorMsg === "Username not found") setEmailError(errorMsg)
+  //         if(errorMsg === "Incorrect password provided") setPassError(errorMsg)
+  //     }
+  // }
 
 
   return (
