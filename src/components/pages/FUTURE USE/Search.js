4648f9cd3919e6d85fbd5ddb1ce90738e6774802
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-12-30 10:13:21 -0600

    Search Function -- now can search by title or keyword.

--- src/components/pages/FUTURE USE/Search.js
+++ src/components/pages/FUTURE USE/Search.js
@@ -30,10 +30,17 @@ function Search(props) {
     }, [])
 
 
-        const handleSearchSubmit = (e) => {
+    //     const handleSearchSubmit = (e) => {
+    //     e.preventDefault()
+    //     const searchResults = components.filter(component => {
+    //         return component.keywords.toLowerCase().includes(searchTerm.toLowerCase())
+    //     })
+    //     setFilteredResults(searchResults)
+    // }
+    const handleSearchSubmit = (e) => {
         e.preventDefault()
         const searchResults = components.filter(component => {
-            return component.keywords.toLowerCase().includes(searchTerm.toLowerCase())
+            return component.keywords.toLowerCase().includes(searchTerm.toLowerCase()) || component.name.toLowerCase().includes(searchTerm.toLowerCase())
         })
         setFilteredResults(searchResults)
     }
