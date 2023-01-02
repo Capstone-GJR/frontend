ea5134cb3a0bfc09ae83a68f8ef6f41ce9e7a94f
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-02 13:47:13 -0600

    Add .

--- src/components/pages/general/Welcome.js
+++ src/components/pages/general/Welcome.js
@@ -11,7 +11,7 @@ function Welcome() {
           <h1>TRAQURA</h1>
         </div>
             <div className="row mt-3 mb-3 w-auto">
-                <p className="text-center fs-5">Traqura is the smart(er) solution for home inventory and personal organization. This web app allows users to transform their mobile device into a powerful inventory management tool where they can catalogue their items, their value, and track them via self-assigned locations and totes</p>
+                <p className="text-center fs-5">Traqura is the smart(er) solution for home inventory and personal organization. This web app allows users to transform their mobile device into a powerful inventory management tool where they can catalogue their items, their value, and track them via self-assigned locations and totes.</p>
             </div>
       </div>
       <div className='row justify-content-center my-2'>
