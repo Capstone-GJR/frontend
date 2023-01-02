62f7c0a7d03a9678b6363b632fe1c8b3687e5323
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-02 14:05:06 -0600

    max width for paragraph

--- src/components/pages/general/Welcome.js
+++ src/components/pages/general/Welcome.js
@@ -5,13 +5,13 @@ import LargeNavbar from "../../navbar/LargeNavbar";
 
 function Welcome() {
   return (
-    <div className='container'>
+    <div className='container col'> 
         <div className='row header' style={{marginBottom:"15em"}}>
         <div className='col text-center'>
           <h1>TRAQURA</h1>
         </div>
             <div className="row mt-3 mb-3 w-auto">
-                <p className="text-center fs-5">Traqura is the smart(er) solution for home inventory and personal organization. This web app allows users to transform their mobile device into a powerful inventory management tool where they can catalogue their items, their value, and track them via self-assigned locations and totes.</p>
+                <p className="text-center fs-5 col col-md-8 col-lg-6 ms-auto me-auto">Traqura is the smart(er) solution for home inventory and personal organization. This web app allows users to transform their mobile device into a powerful inventory management tool where they can catalogue their items, their value, and track them via self-assigned locations and totes.</p>
             </div>
       </div>
       <div className='row justify-content-center my-2'>
