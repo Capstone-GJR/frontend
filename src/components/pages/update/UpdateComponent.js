6aa4a5ff6ed4ae400084685e5333a848ed9bcfbd
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-03 15:05:07 -0600

    Fix nav link

--- src/components/pages/update/UpdateComponent.js
+++ src/components/pages/update/UpdateComponent.js
@@ -11,9 +11,7 @@ function UpdateComponent(props) {
     return (
             <div className="pgContainer maxWidth600">
                 <h1>{props.userObject.component.name}</h1>
-                {/*<p>Keywords: {props.userObject.component.keywords}</p>*/}
                 <EditForm props {...props} />
-
             </div>
     )
 }
