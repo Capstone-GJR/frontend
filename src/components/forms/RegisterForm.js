64b0d29f97af1813c3bd1c76460f007ebcb88011
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-04 14:03:40 -0600

    password length error handled

--- src/components/forms/RegisterForm.js
+++ src/components/forms/RegisterForm.js
@@ -7,20 +7,21 @@ import { useNavigate } from "react-router-dom";
 //TODO: Error Handling for if email is already in use
 function RegisterForm() {
   const navigate = useNavigate();
-  const [form, setForm] = 
-  useState({
-    firstName:'', 
-    lastName:'', 
-    email:'', 
-    password:'', 
-    password2:''
-  });
+  const [form, setForm] =
+      useState({
+        firstName:'',
+        lastName:'',
+        email:'',
+        password:'',
+        password2:''
+      });
 
   const [errors, setErrors] = useState({});
 
   const regex = /\S+@\S+\.\S+/;
   const blankErrorMsg = 'Field can not be blank';
   const emailErrorMsg = 'Please enter a valid email';
+  const passwordLengthError = 'Password must be at least 8 characters'
 
   // Function to set form values from user input
   const setField = (field, value) => {
@@ -34,7 +35,7 @@ function RegisterForm() {
       [field]:null
     })
   }
-  
+
   const validateForm = (emailMsg) => {
     const { firstName, lastName, email, password, password2 } = form;
     const newErrors = {}
@@ -42,6 +43,7 @@ function RegisterForm() {
     if(!firstName || firstName === '') newErrors.firstName = blankErrorMsg
     else if (!lastName || lastName === '') newErrors.lastName = blankErrorMsg
     else if (!email || email === '' || !regex.test(email)) newErrors.email = emailErrorMsg
+    else if (password.trim().length < 8) newErrors.password = passwordLengthError
     else if (!password || password === '') newErrors.password = blankErrorMsg
     else if (!password2 || password2 === '') newErrors.password2 = blankErrorMsg
     else if (password !== password2) newErrors.password2 = "Passwords do not match"
@@ -66,104 +68,104 @@ function RegisterForm() {
         email: form.email,
         password: form.password
       })
-      .then((res) => {
-        setForm({
-          firstName:'',
-          lastName:'', 
-          email:'', 
-          password:'', 
-          password2:''
-      });
-        navigate("/login")
-        console.log(res);
-      })
-      .catch((error) => {
-        console.log(error);
-        if(error.response.data.message === "The email is already in use") {
-          setErrors({email:error.response.data.message});
-        }
-      });
+          .then((res) => {
+            setForm({
+              firstName:'',
+              lastName:'',
+              email:'',
+              password:'',
+              password2:''
+            });
+            navigate("/login")
+            console.log(res);
+          })
+          .catch((error) => {
+            console.log(error);
+            if(error.response.data.message === "The email is already in use") {
+              setErrors({email:error.response.data.message});
+            }
+          });
     }
   }
 
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="firstName">
-        <Form.Label className="mx-1" >FIRST NAME</Form.Label>
-        <Form.Control 
-          className="p-3 inputText" 
-          type="text" 
-          placeholder="first name" 
-          value={form.firstName}
-          onChange={(e) => setField("firstName", e.target.value)}
-          isInvalid={!!errors.firstName}
+      <Form>
+        <Form.Group className="mb-3" controlId="firstName">
+          <Form.Label className="mx-1" >FIRST NAME</Form.Label>
+          <Form.Control
+              className="p-3 inputText"
+              type="text"
+              placeholder="first name"
+              value={form.firstName}
+              onChange={(e) => setField("firstName", e.target.value)}
+              isInvalid={!!errors.firstName}
           />
-        <Form.Control.Feedback type='invalid' className="text-center">
-          {errors.firstName}
-        </Form.Control.Feedback>
-      </Form.Group>
+          <Form.Control.Feedback type='invalid' className="text-center">
+            {errors.firstName}
+          </Form.Control.Feedback>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="lastName">
-        <Form.Label className="mx-1" >LAST NAME</Form.Label>
-        <Form.Control 
-          className="p-3 inputText"
-          type="text" 
-          placeholder="last name" 
-          value={form.lastName}
-          onChange={(e) => setField("lastName", e.target.value)}
-          isInvalid={!!errors.lastName}
+        <Form.Group className="mb-3" controlId="lastName">
+          <Form.Label className="mx-1" >LAST NAME</Form.Label>
+          <Form.Control
+              className="p-3 inputText"
+              type="text"
+              placeholder="last name"
+              value={form.lastName}
+              onChange={(e) => setField("lastName", e.target.value)}
+              isInvalid={!!errors.lastName}
           />
-        <Form.Control.Feedback type="invalid" className="text-center">
-          {errors.lastName}
-        </Form.Control.Feedback>
-      </Form.Group>
+          <Form.Control.Feedback type="invalid" className="text-center">
+            {errors.lastName}
+          </Form.Control.Feedback>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label className="mx-1" >EMAIL</Form.Label>
-        <Form.Control 
-          className="p-3 inputText" 
-          type="email" 
-          placeholder="email@email.com" 
-          value={form.email}
-          onChange={(e) => setField("email", e.target.value)}
-          isInvalid={!!errors.email}
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label className="mx-1" >EMAIL</Form.Label>
+          <Form.Control
+              className="p-3 inputText"
+              type="email"
+              placeholder="email@email.com"
+              value={form.email}
+              onChange={(e) => setField("email", e.target.value)}
+              isInvalid={!!errors.email}
           />
-        <Form.Control.Feedback type="invalid" className="text-center">
-          {errors.email}
-        </Form.Control.Feedback>
-      </Form.Group>
+          <Form.Control.Feedback type="invalid" className="text-center">
+            {errors.email}
+          </Form.Control.Feedback>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="password">
-        <Form.Label className="mx-1">PASSWORD</Form.Label>
-        <Form.Control 
-          className="p-3 inputText" 
-          type="password" 
-          placeholder="enter password" 
-          value={form.password}
-          onChange={(e) => setField("password", e.target.value)}
-          isInvalid={!!errors.password}
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label className="mx-1">PASSWORD</Form.Label>
+          <Form.Control
+              className="p-3 inputText"
+              type="password"
+              placeholder="enter password"
+              value={form.password}
+              onChange={(e) => setField("password", e.target.value)}
+              isInvalid={!!errors.password}
           />
-        <Form.Control.Feedback type="invalid" className="text-center">
-          {errors.password}
-        </Form.Control.Feedback>
-      </Form.Group>
+          <Form.Control.Feedback type="invalid" className="text-center">
+            {errors.password}
+          </Form.Control.Feedback>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="password2">
-        <Form.Label className="mx-1">CONFIRM PASSWORD</Form.Label>
-        <Form.Control 
-          className="p-3 inputText" 
-          type="password" 
-          placeholder="password" 
-          value={form.password2}
-          onChange={(e) => setField("password2", e.target.value)}
-          isInvalid={!!errors.password2}
+        <Form.Group className="mb-3" controlId="password2">
+          <Form.Label className="mx-1">CONFIRM PASSWORD</Form.Label>
+          <Form.Control
+              className="p-3 inputText"
+              type="password"
+              placeholder="password"
+              value={form.password2}
+              onChange={(e) => setField("password2", e.target.value)}
+              isInvalid={!!errors.password2}
           />
-        <Form.Control.Feedback type="invalid" className="text-center">
-          {errors.password2}
-        </Form.Control.Feedback>
-      </Form.Group>
-      <Button title="SIGN UP" onClick={handleSubmit} />
-    </Form>
+          <Form.Control.Feedback type="invalid" className="text-center">
+            {errors.password2}
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button title="SIGN UP" onClick={handleSubmit} />
+      </Form>
   );
 }
 
