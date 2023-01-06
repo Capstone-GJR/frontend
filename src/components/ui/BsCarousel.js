9444105af0471ba18ad3ab7b34a77c537c648a2d
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-06 14:54:37 -0600

    completed carousel edits

--- src/components/ui/BsCarousel.js
+++ src/components/ui/BsCarousel.js
@@ -1,5 +1,10 @@
 import Carousel from 'react-bootstrap/Carousel';
-import photo1 from "../../assets/photo1.png"
+import photo1 from "../../assets/photo1.jpeg"
+import photo2 from "../../assets/photo2.jpeg";
+import photo3 from "../../assets/photo3.png";
+import photo4 from "../../assets/photo4.jpeg";
+import photo5 from "../../assets/photo5.jpeg";
+import photo6 from "../../assets/photo6.jpeg";
 
 function BsCarousel() {
   return (
@@ -14,14 +19,35 @@ function BsCarousel() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={photo1}
+          src={photo2}
           alt="Second slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={photo1}
+          src={photo3}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={photo4}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={photo5}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={photo6}
           alt="Third slide"
         />
       </Carousel.Item>
